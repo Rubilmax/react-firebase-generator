3410ea664974dbcebe126aeb6d074a7b6ab9e952
rubilmax <rmilon@gmail.com>
2021-02-14 17:37:24 +0100

    Updated services

--- src/services/style.ts
+++ src/services/style.ts
@@ -1,5 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+export const colors = {
+  primary: '#1a3668',
+  darkPrimary: '#0a1529',
+  secondary: '#e9ede2',
+  darkBlue: '#232232',
+};
+
 export const useColors = makeStyles({
   lightGrey: {
     color: '#ddd',
--- src/services/theme.ts
+++ src/services/theme.ts
@@ -1,8 +1,22 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+import { colors } from 'services/style';
+
+export const mobileMediaQuery = '(max-width: 450px)';
+export const desktopMediaQuery = '(min-width: 850px)';
+export const useIsMobile = () => useMediaQuery(mobileMediaQuery);
 
 const createTheme = () =>
   createMuiTheme({
-    palette: {},
+    palette: {
+      primary: {
+        main: colors.primary,
+      },
+      secondary: {
+        main: colors.secondary,
+      },
+    },
     overrides: {},
   });
 
